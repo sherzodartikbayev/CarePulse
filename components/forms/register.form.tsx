9ceed65497481c53cd4e8fc8e3d4d00252df0826
Sherzod Artikbayev <info@web-pro.uz>
2025-12-07 21:02:49 +0500

    feat: Personal Information Form of Register Page

---
+++ components/forms/register.form.tsx
@@ -0,0 +1,142 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form, FormControl } from '@/components/ui/form'
+import CustomFormField from '../shared/custom-form-field'
+import SubmitButton from '../shared/submit-button'
+import { useState } from 'react'
+import { UserFormValidation } from '@/lib/validation'
+import { useRouter } from 'next/navigation'
+import { createUser } from '@/lib/actions/patient.actions'
+import { FormFieldType } from './patient.form'
+import { RadioGroup, RadioGroupItem } from '../ui/radio-group'
+import { GenderOptions } from '@/constants'
+import { Label } from '../ui/label'
+
+export function RegisterForm({ user }: { user: User }) {
+	const router = useRouter()
+	const [isLoading, setIsLoading] = useState(false)
+
+	const form = useForm<z.infer<typeof UserFormValidation>>({
+		resolver: zodResolver(UserFormValidation),
+		defaultValues: {
+			name: '',
+			email: '',
+			phone: '',
+		},
+	})
+
+	async function onSubmit({
+		name,
+		email,
+		phone,
+	}: z.infer<typeof UserFormValidation>) {
+		setIsLoading(true)
+
+		try {
+			const userData = {
+				name,
+				email,
+				phone,
+			}
+			const user = await createUser(userData)
+			if (user) router.push(`/patients/${user.$id}/register`)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	return (
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className='space-y-12 flex-1'
+			>
+				<section className='space-y-4'>
+					<h1 className='header'>Welcome 👋</h1>
+					<p className='text-dark-700'>Schedule your first appointment.</p>
+				</section>
+
+				<section className='mt-9 space-y-1'>
+					<h2 className='sub-header'>Personal Information</h2>
+				</section>
+
+				<CustomFormField
+					fieldType={FormFieldType.INPUT}
+					control={form.control}
+					name='name'
+					label='Full Name'
+					placeholder='John Doe'
+					iconSrc='/assets/icons/user.svg'
+					iconAlt='user'
+				/>
+
+				<div className='flex flex-col gap-6 xl:flex-row'>
+					<CustomFormField
+						fieldType={FormFieldType.INPUT}
+						control={form.control}
+						name='email'
+						label='Email'
+						placeholder='example@mail.com'
+						iconSrc='/assets/icons/email.svg'
+						iconAlt='email'
+					/>
+
+					<CustomFormField
+						fieldType={FormFieldType.PHONE_INPUT}
+						control={form.control}
+						name='phone'
+						label='Phone Number'
+						placeholder='+00 0342 0453 34'
+					/>
+				</div>
+
+				<div className='flex flex-col gap-6 xl:flex-row'>
+					<CustomFormField
+						fieldType={FormFieldType.DATA_PICKER}
+						control={form.control}
+						name='birthDate'
+						label='Date of birth'
+					/>
+
+					<CustomFormField
+						fieldType={FormFieldType.SKELETON}
+						control={form.control}
+						name='gender'
+						label='Gender'
+						renderSkeleton={field => (
+							<FormControl>
+								<RadioGroup
+									className='flex h-11 gap-6 xl:justify-between'
+									onValueChange={field.onChange}
+									defaultValue={field.value}
+								>
+									{GenderOptions.map(option => (
+										<div className='radio-group' key={option}>
+											<RadioGroupItem value={option} id={option} />
+											<Label htmlFor={option} className='cursor-pointer'>
+												{option}
+											</Label>
+										</div>
+									))}
+								</RadioGroup>
+							</FormControl>
+						)}
+					/>
+				</div>
+
+				<div className='flex flex-col gap-6 xl:flex-row'></div>
+
+				<div className='flex flex-col gap-6 xl:flex-row'></div>
+
+				<div className='flex flex-col gap-6 xl:flex-row'></div>
+
+				<div className='flex flex-col gap-6 xl:flex-row'></div>
+
+				<SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+			</form>
+		</Form>
+	)
+}
--- components/shared/custom-form-field.tsx
+++ components/shared/custom-form-field.tsx
@@ -15,6 +15,8 @@ import Image from 'next/image'
 import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input'
 import { E164Number } from 'libphonenumber-js/core'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 interface CustomProps {
 	control: Control<any>
@@ -32,7 +34,15 @@ interface CustomProps {
 }
 
 const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
-	const { fieldType, iconSrc, iconAlt, placeholder } = props
+	const {
+		fieldType,
+		iconSrc,
+		iconAlt,
+		placeholder,
+		showTimeSelect,
+		dateFormat,
+		renderSkeleton,
+	} = props
 
 	switch (fieldType) {
 		case FormFieldType.INPUT:
@@ -73,13 +83,40 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
 				</FormControl>
 			)
 
+		case FormFieldType.DATA_PICKER:
+			return (
+				<div className='flex rounded-md border border-dark-500 bg-dark-400'>
+					<Image
+						src='/assets/icons/calendar.svg'
+						height={24}
+						width={24}
+						alt='calendar'
+						className='ml-2'
+					/>
+
+					<FormControl>
+						<DatePicker
+							selected={field.value}
+							onChange={date => field.onChange(date)}
+							dateFormat={dateFormat ?? 'MM/dd/yyyy'}
+							showDateSelect={showTimeSelect ?? false}
+							timeInputLabel='Time:'
+							wrapperClassName='date-picker'
+						/>
+					</FormControl>
+				</div>
+			)
+
+		case FormFieldType.SKELETON:
+			return renderSkeleton ? renderSkeleton(field) : null
+
 		default:
 			break
 	}
 }
 
 const CustomFormField = (props: CustomProps) => {
-	const { control, fieldType, name, label } = props
+	const { control, name, label } = props
 
 	return (
 		<FormField
@@ -87,9 +124,7 @@ const CustomFormField = (props: CustomProps) => {
 			name={name}
 			render={({ field }) => (
 				<FormItem className='flex-1'>
-					{fieldType === FormFieldType.INPUT && label && (
-						<FormLabel>{label}</FormLabel>
-					)}
+					{label && <FormLabel>{label}</FormLabel>}
 
 					<RenderField field={field} props={props} />
 
